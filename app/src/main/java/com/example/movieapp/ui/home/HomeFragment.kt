674986c96560eb8ec2985e81be911885484699b8
manhvu2209 <manh.vu@gumiviet.com>
2021-06-08 20:26:39 +0700

    - feat(home_screen): create Home Screen, bottom navigation bar.

---
+++ app/src/main/java/com/example/movieapp/ui/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp.ui.home
+
+import com.example.movieapp.R
+import com.example.movieapp.base.BaseFragment
+
+class HomeFragment: BaseFragment() {
+    override fun getLayoutID(): Int {
+        return R.layout.fragment_home
+    }
+
+    override fun doViewCreated() {
+
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/ui/search/SearchFragment.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp.ui.search
+
+import com.example.movieapp.R
+import com.example.movieapp.base.BaseFragment
+
+class SearchFragment: BaseFragment() {
+    override fun getLayoutID(): Int {
+        return R.layout.fragment_search
+    }
+
+    override fun doViewCreated() {
+
+    }
+}
